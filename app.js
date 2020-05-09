1693251383bb29c55993367aa12f87ec94843e27
Mona Lisa <david.t.bui83@gmail.com>
2020-05-08 22:46:40 -0700

    connect to mongodb

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const app = express();
 const port = 7000;
+
+
+// // Map global promise - get rid of warning
+// mongoose.Promise = global.Promise;
+
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {console.log('MongoDB connected...')})
+.catch(err => console.log(err));
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
