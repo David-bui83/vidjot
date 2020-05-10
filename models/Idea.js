7cfbdab0953be9b6cce0406d7f091875e994481e
Mona Lisa <david.t.bui83@gmail.com>
2020-05-09 20:28:37 -0700

    add idea, edit idea

--- models/Idea.js
+++ models/Idea.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 // Create Schema
 const IdeaSchema = new Schema({
   title:{type: String, required: true},
-  date:{type: String, required: true},
+  details:{type: String, required: true},
   date:{type: Date, default: Date.now}
 });
 
