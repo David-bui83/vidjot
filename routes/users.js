3646bddd1a16d65e7e3c0fe3c0d61e026a060b7f
Mona Lisa <david.t.bui83@gmail.com>
2020-05-10 17:43:09 -0700

    added authentication middleware

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,6 @@ const bcrypt = require('bcryptjs');
 const passport = require('passport')
 const router = express.Router();
 
-
 // Load User Model
 require('../models/User');
 const User = mongoose.model('users');
